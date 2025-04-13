932f294720f956e24df371dca2815f2f34013292
Pedro C. de Souza <pedro.cpnsz@gmail.com>
2025-04-12 23:22:36 -0300

    Angular inicializado

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 import google.generativeai as genai
+from google.api_core.exceptions import GoogleAPIError
 
 app = Flask(__name__)
 
 genai.configure(api_key="AIzaSyDb9n7aIMKRnuQ0pOE1PrZq41h4KNJu0TY")
 
-model = genai.GenerativeModel('gemini-1.5-pro-latest')
+# Define o modelo (versão) a ser utilizado
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 # Inicia uma conversa com o modelo e define o histórico inicial (vazio)
 chat = model.start_chat(history=[])
@@ -14,11 +16,21 @@ chat = model.start_chat(history=[])
 @app.route('/prompt', methods=['POST'])
 # Recebe o prompt do cliente e envia para o modelo
 def gerar_resposta():
-    data = request.json
-    prompt = data.get("prompt", "")
-    response = chat.send_message(prompt)
-    # Retorna a resposta do modelo como JSON
-    return jsonify({"response": response.text})
+    try:
+        data = request.json
+        prompt = data.get("prompt", "")
+        # Verifica se o prompt não está vazio
+        if not prompt:
+            return jsonify({"error": "Prompt vazio."}), 400
+        response = chat.send_message(prompt)
+        # Retorna a resposta do modelo como JSON
+        return jsonify({"response": response.text})
+    # Captura erros específicos da API do Gemini
+    except GoogleAPIError as e:
+        return jsonify({"error": f"Erro com a API do Gemini: {str(e)}"}), 500
+    
+    except Exception as e:
+        return jsonify({"error": f"Erro interno: {str(e)}"}), 500
 
 # Inicia o servidor Flask
 if __name__ == '__main__':
---
+++ backend-trabalho-ia/src/test/java/com/pedrocpnsz/backend_trabalho_ia/BackendTrabalhoIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrocpnsz.backend_trabalho_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTrabalhoIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
